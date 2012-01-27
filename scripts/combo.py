de1a898ac7b4d1918c691de7884373df1c9d02e6
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2012-01-27 10:56:29 +0000

    Log file and error detection added for bad binning on input
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@248720 47dd0619-b7a8-462c-89d8-56395fd97e59

--- scripts/combo.py
+++ scripts/combo.py
@@ -8,6 +8,7 @@
 
 import os
 import sys
+import subprocess
 
 #
 # Build the standard command line arguments to the FT guy given
@@ -22,18 +23,29 @@ def buildArgs(config):
     return cmdfile
 
 #
-# Run the full thing
+# Run a command line and return the data
 #
-def doCombo(name):
+def runProc(cmdline):
+    args = cmdline.split()
+    p = subprocess.Popen(args, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+    text = p.communicate()[0]
+    errorcode = p.returncode
 
-    #
-    # load in the module for name so we can get all the config info
-    # it will search sys.path for the module...
-    #
+    return errorcode, text
+
+#
+# Dump out some html about how it went
+#
+def dumpResultSection (html, err, text, title):
+    print >> html, "<h1>%s</h1>" % title
+    print >> html, "<PRE>%s</PRE>" % text
+    print >> html, "result code: %d" % err
+
+#
+# Do the actual combo and root file building here
+#
+def doComboImpl (configInfo, html):
     
-    configInfo = __import__ (name)
-    print configInfo.title
-
     #
     # Build the standard command line
     #
@@ -44,19 +56,55 @@ def doCombo(name):
     # Get a list of the names
     #
 
-    result = os.system("FTDump.exe %s --names" % stdCmdArgs)
+    errcode, result = runProc("FTDump.exe %s --names" % stdCmdArgs)
+    dumpResultSection (html, errcode, result, "All analyses to be processed")
 
     #
     # First job is to "check" the file to make sure it is ok.
     #
 
-    result = os.system("FTDump.exe %s --check" % stdCmdArgs)
-    print result
+    errcode, result = runProc("FTDump.exe %s --check" % stdCmdArgs)
+    dumpResultSection (html, errcode, result, "Bin Consistency Check")
+
+    if errcode <> 0:
+        print "consitancy check failed, stopping now..."
+        return
+
+#
+# Run the full thing
+#
+def doCombo(name):
+    #
+    # load in the module for name so we can get all the config info
+    # it will search sys.path for the module...
+    #
+    
+    configInfo = __import__ (name)
+    print configInfo.title
+
+    #
+    # Dump what happens to a html file as we go...
+    #
+
+    html = open("%s.html" % name, "w")
+    print >> html, "<html><header><title>%s</title></header><body>" % configInfo.title
+
+    #
+    # Do the work
+    #
+
+    doComboImpl(configInfo, html)
+
+    #
+    # Done with the log...
+    #
+
+    print >> html, "</body></html>"
 
 #
 # If this is invoked from the command line..
 #
 
 if __name__ == "__main__":
-    doCombo("MV180")
+    doCombo("MV160")
 
