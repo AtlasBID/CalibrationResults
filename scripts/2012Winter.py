c14652cb13d223ec625499f5c93605845cacf19a
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2012-02-08 17:05:29 +0000

    Working towards code that can do a full combination
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@254979 47dd0619-b7a8-462c-89d8-56395fd97e59

--- scripts/2012Winter.py
+++ scripts/2012Winter.py
@@ -15,6 +15,7 @@ inputs = [
     "analyses/2012-Winter/sv0mass/*.txt",
     "analyses/2012-Winter/ptrel/*.txt",
     "analyses/2012-Winter/negativetag/*.txt",
+    "analyses/2012-Winter/stat_correlation_inputs.txt"
     ]
 #inputs = "analyses/System8-MV160.txt analyses/ptrel-MV160.txt analyses/ptrel-JFITNN70.txt analyses/stat_correlation_inputs.txt"
 #inputs = "analyses/pTrel-JFITNN70.txt"
@@ -37,8 +38,12 @@ for t in taggers:
 # List of guys that we are going to ignore during our
 # running
 #
-ignore_analyses = []
-
+ignore_analyses = [
+    "pTrel-bottom-.*-AntiKt4Topo:20-pt-200:0-abseta-0.6", 
+    "pTrel-bottom-.*-AntiKt4Topo:20-pt-200:0.6-abseta-1.2",
+    "pTrel-bottom-.*-AntiKt4Topo:20-pt-200:1.2-abseta-1.8",
+    "pTrel-bottom-.*-AntiKt4Topo:20-pt-200:1.8-abseta-2.5",
+    ]
 
 #
 # The root file that we start everything with.
