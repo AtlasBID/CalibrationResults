ca4f8b6f9f94b329f230b1708e73b22c693f1a06
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2012-11-20 13:05:55 +0000

    Getting the noJVF stuff working
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@290881 47dd0619-b7a8-462c-89d8-56395fd97e59

---
+++ analyses/D2012-HCP/CombineConfig-noJVF.py
@@ -0,0 +1,91 @@
+#
+# Config for doing the MV180 combination and file generation for dijets only.
+#
+
+title = "2012 dataset noJVF B-Tagging SF Results"
+description = "Uses HCP 2012 ptRel inputs. Uses early 2012 DStar and negative tag results. D* LC inputs are a straight copy of EM inputs."
+name = "D2012-HCP-noJVF"
+
+#
+# Input arguments
+#
+
+inputs = [
+    "ptrel/noJVF/*.txt",
+
+    "DStar/noJVF/*.txt",
+    "ptrel/noJVF/CharmTau/*.txt",
+
+    "negativetag/noJVF/*topoEM_v3.txt",
+    "negativetag/noJVF/*topoLC_v3.txt",
+
+    "defaults.txt",
+    ]
+
+taggersEM = [
+    ]
+
+taggersLC = [
+    ]
+
+DoOnlyTaggers = []
+for t in taggersEM:
+    d = {
+        "flavor":"*",
+        "tagger":t[0],
+        "op":t[1],
+        "jet":"AntiKt4TopoEM"
+        }
+    DoOnlyTaggers.append(d)
+
+for t in taggersLC:
+    d = {
+        "flavor":"*",
+        "tagger":t[0],
+        "op":t[1],
+        "jet":"AntiKt4TopoLC"
+        }
+    DoOnlyTaggers.append(d)
+    
+#
+# Optional analysis groupings. This is used if more than one
+# grouping of combination is desired. The results are all put in the
+# same final file, of course. If a flavor entry is missing, then everything
+# for that flavor is done. Otherwise, *only* what is listed is done. The empty
+# [], btw, means everything should be done.
+#
+
+analysisGroupings = {
+    }
+
+#
+# List of guys that we are going to ignore during our
+# running
+#
+ignore_analyses = [
+    # Drop the first bin in the ttbar
+    ".*:25-pt-30:.*",
+
+    # Drop the extra bins in the pTrel analysis
+    "pTrel-.*20-pt-200.*",
+
+    # Drop anything to do with the MV2 tagger
+    ".*MV2.*",
+
+    ]
+
+#
+# The root file that we start everything with.
+#
+
+mcEffRootFile = [
+    "TopCalibrations_rel17_MC12a_EM_Eff_nojvfCut_Convert.root",
+    "TopCalibrations_rel17_MC12a_LC_Eff_nojvfCut_Convert.root",
+    ]
+CDIFile = "%s-rel17_MC12a_D2012_HCP.root" % name
+
+#
+# Should we avoid the combination?
+#
+
+runCombination = True
