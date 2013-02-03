62cf5b8f817f9b89508f656c9e6176b5be453c45
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2013-02-03 21:25:56 +0000

    Remove neg tags - they aren't ready yet for prime-time.
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@291711 47dd0619-b7a8-462c-89d8-56395fd97e59

--- analyses/2013-Winter/2013Winter.py
+++ analyses/2013-Winter/2013Winter.py
@@ -13,7 +13,8 @@ description = "Winter 2013 results for dijet and ttbar, based on 2012 data"
 
 inputs = [
     "pTRel/*.txt",
-    "negativetag/*.txt",
+
+#    "negativetag/*.txt",
     
 #    "DStar/*.txt",
 #    "system8/*.txt",
