e3c0decc00cc2f35e5420335950a7e954598a703
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2013-05-06 07:07:42 +0000

    Get S8 back into the mix, with rebinning for a compare
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@292754 47dd0619-b7a8-462c-89d8-56395fd97e59

--- analyses/2013-LHCP/2013LHCP-new.py
+++ analyses/2013-LHCP/2013LHCP-new.py
@@ -14,15 +14,44 @@ name = "2013-LHCP"
 description = "Spring 2013 results, based on the full 2012 data, for LHCP"
 
 #
+# Legal taggers. These will be used lower down to filter out the inputs to make sure
+# we are dealing only with what we want to deal with.
+#
+
+taggers = [
+#MV1 60% JVF (EM, LC), noJVF (EM, LC)
+    ["MV1", "0.9867"], 
+    ["MV1", "0.9827 "], 
+#MV1 70%
+    ["MV1", "0.8119"], 
+    ["MV1", "0.7892"], 
+#MV1 80%
+    ["MV1", "0.3900"], 
+    ["MV1", "0.3511"], 
+#MV1c 50% JVF (EM, LC)
+    ["MV1c", "0.9237"], 
+    ["MV1c", "0.9195"], 
+#MV1c 57% JVF (EM, LC)
+    ["MV1c", "0.8674"], 
+    ["MV1c", "0.8641"], 
+	
+    ]
+#
 # Input arguments
 #
 
-s8 = files("system8/*.txt")
+s8 = files("system8/*.txt") \
+     .filter(taggers=taggers) \
+     .filter(ignore=[".*JVF0_5.*"])
+
 ttdilep_emu_pdf = files("pdfmethod_ttdilep/*.txt") \
-                  .filter(["PDFmethod_dilepton_emu_3jets", "PDFmethod_dilepton_emu_2jets"])
+                  .filter(analyses = ["PDFmethod_dilepton_emu_3jets", "PDFmethod_dilepton_emu_2jets"]) \
+                  .filter(taggers=taggers)
 ttdilep_ll_pdf = files("pdfmethod_ttdilep/*.txt") \
-                  .filter(["PDFmethod_dilepton_ll_3jets", "PDFmethod_dilepton_ll_2jets"])
-ttdilep_topo = files("topo_ttemu/*.txt")
+                 .filter(analyses = ["PDFmethod_dilepton_ll_3jets", "PDFmethod_dilepton_ll_2jets"]) \
+                 .filter(taggers=taggers)
+ttdilep_topo = files("topo_ttemu/*.txt") \
+               .filter(taggers=taggers)
 
 #
 # Some checks
@@ -41,48 +70,25 @@ fit_ttdilep_emu_pdf = ttdilep_emu_pdf.fit("PDFmethod_dilepton_emu_fit")
 #
 # Put the fit_ttdilep and S8 on equal footing - for plotting purposes only.
 #
-#rebin_template = files("commonbinning.txt")
-#fit_ttdilep_s8_binning = (fit_ttdilep_ll_pdf + rebin_template) \
-#                         .rebin("rebin", "PDFmethod_dilepton_ll_fit_rebin")
-#s8_rebinned = (rebin_template + s8) \
-#              .rebin("rebin", "system8_rebin")
+rebin_template = files("commonbinning.txt")
+fit_ttdilep_s8_binning = (fit_ttdilep_ll_pdf + rebin_template) \
+                         .rebin("rebin", "PDFmethod_dilepton_ll_fit_rebin")
+s8_rebinned = (rebin_template + s8) \
+              .rebin("rebin", "system8_rebin")
 
 #
-# Plots
+# Plot the two fits so we can compare them (well).
 #
 
 (fit_ttdilep_ll_pdf + fit_ttdilep_emu_pdf).plot("pdf_method_fits")
 (ttdilep_ll_pdf + ttdilep_emu_pdf + fit_ttdilep_ll_pdf + fit_ttdilep_emu_pdf).plot("pdf_method_all")
-#(fit_ttdilep_s8_binning + s8_rebinned).plot("tts8_compare")
 
-taggers = [
-#    ["IP3DSV1", "4.55"],
-#    ["IP3DSV1", "1.70"],
-#    ["IP3DSV1", "-0.80"],
-#    ["JetFitterCOMBNN", "2.20"],
-#    ["JetFitterCOMBNN", "1.80"],
-#    ["JetFitterCOMBNN", "0.35"],
-#    ["JetFitterCOMBNN", "-1.25"],
-#    ["JetFitterCOMBNNc", "1.33"],
-#    ["JetFitterCOMBNNc", "0.98"],
+#
+# Plot the dijet and the main ttbar fit for comparison as well
+#
 
-#MV1 60% JVF (EM, LC), noJVF (EM, LC)
-    ["MV1", "0.9867"], 
-    ["MV1", "0.9827 "], 
-#MV1 70%
-    ["MV1", "0.8119"], 
-    ["MV1", "0.7892"], 
-#MV1 80%
-    ["MV1", "0.3900"], 
-    ["MV1", "0.3511"], 
-#MV1c 50% JVF (EM, LC)
-    ["MV1c", "0.9237"], 
-    ["MV1c", "0.9195"], 
-#MV1c 57% JVF (EM, LC)
-    ["MV1c", "0.8674"], 
-    ["MV1c", "0.8641"], 
-	
-    ]
+(fit_ttdilep_s8_binning + s8_rebinned).plot("tts8_compare")
+
 
 TrigTagBuilder = [
 #    ["", "-999"],
