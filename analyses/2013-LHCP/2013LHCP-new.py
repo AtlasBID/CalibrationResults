7b4bd8b088c5ca46ab784073bc507e3ad486817e
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2013-05-14 22:07:12 +0000

    Dump out a sys error table
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@292869 47dd0619-b7a8-462c-89d8-56395fd97e59

--- analyses/2013-LHCP/2013LHCP-new.py
+++ analyses/2013-LHCP/2013LHCP-new.py
@@ -155,6 +155,7 @@ master_cdi_file = \
     + negative
 master_cdi_file.make_cdi("MC12-CDI", "defaults.txt", "MCefficiencies_for_CDI_14.4.2013.root")
 master_cdi_file.plot("MC12-CDI")
+master_cdi_file.dump(sysErrors = True)
 
 #
 # The root file that we start everything with.
--- scripts/dump.py
+++ scripts/dump.py
@@ -10,8 +10,8 @@ import comboGlobals
 # We will filter out input files on some criteria
 #
 
-def dump(sfObj, check=False):
-    fc = Dump(sfObj, check)
+def dump(sfObj, check=False, sysErrors = False):
+    fc = Dump(sfObj, check, sysErrors)
     comboGlobals.Commands += [fc]
     return sfObj
 
@@ -20,9 +20,10 @@ def dump(sfObj, check=False):
 #
 
 class Dump:
-    def __init__ (self, sfinfo, check):
+    def __init__ (self, sfinfo, check, sysErrors):
         self._sf = sfinfo
         self._check = check
+        self._sysErrors = sysErrors
 
     def Execute (self, html, configInfo):
         files = listToString(self._sf.ResolveToFiles(html))
@@ -31,4 +32,12 @@ class Dump:
             errcod = dumpCommandResult(html, "FTDump.exe --names --check %s" % files, "Checking input files.")
             if errcod != 0:
                 print >> html, "Failed to run check! Command line: %s" % files
+
+        if self._sysErrors:
+            outputName = "%s-%s.html" % (configInfo.name, hash(files))
             
+            errcod = dumpCommandResult(html, "FTDump.exe --sysErrorTable %s output %s" % (files, outputName), "Checking input files.")
+            if errcod != 0:
+                print >> html, "Failed to run check! Command line: %s" % files
+            else:
+                print >> html, '<p><a href="%s">Systematic Error Table</a></p>' % outputName
