b78feada412e16854a53709d9f8ed76e74705130
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2013-09-06 17:47:34 +0000

    Add tagger trend plots for everything.
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@295383 47dd0619-b7a8-462c-89d8-56395fd97e59

--- analyses/2013-LP/2013LP.py
+++ analyses/2013-LP/2013LP.py
@@ -241,6 +241,7 @@ master_cdi_file = \
     + sources
 defaultSFs = master_cdi_file.make_cdi("MC12-CDI", "defaults.txt", "MCefficiencies_for_CDI_14.4.2013.root")
 master_cdi_file.plot("MC12-CDI")
+master_cdi_file.plot("MC12-CDI-Tagger-Trends", effOnly=True, byTaggerEff=True)
 master_cdi_file.dump(sysErrors = True, name="master")
 master_cdi_file.dump(metadata = True, name="master-metadata")
 sources.dump(sysErrors = True, name="sources")
