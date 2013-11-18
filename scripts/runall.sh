fb74f0276d66d488214fe1988e59a7c8e1bb9845
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2013-11-18 08:58:20 +0000

    Fix up some syntax errors
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@296288 47dd0619-b7a8-462c-89d8-56395fd97e59

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -2,6 +2,5 @@
 # This script is sourced in the context of the build runner. Place all the configurations you'd like to run
 # here. It is sourced from the base directory of this package.
 #
-
 #./scripts/combine.py --zip analyses/2013-LP/2013LP.py
 ./scripts/combine.py --zip analyses/2012-Paper/2012Paper-new.py
