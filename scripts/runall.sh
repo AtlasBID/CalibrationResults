29f2452ccb4a158a3f593085524e0ab0509dcc77
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2014-02-04 02:42:47 +0000

    Make sure we are running in bash
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@297364 47dd0619-b7a8-462c-89d8-56395fd97e59

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -1,4 +1,4 @@
-#
+#/bin/env bash
 # This script is sourced in the context of the build runner. Place all the configurations you'd like to run
 # here. It is sourced from the base directory of this package.
 #
