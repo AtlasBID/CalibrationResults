94cf4030c46e2a6fedcfb13505503cd894b9a434
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2014-02-04 06:57:21 +0000

    Run only the 8 TeV stuff
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@297366 47dd0619-b7a8-462c-89d8-56395fd97e59

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -5,7 +5,7 @@
 # The "|| true" at the end of the line allows things to progress - the script doesn't just die.
 
 #
-./scripts/combine.py --zip analyses/2014-Winter-7TeV/2014-Winter-7TeV.py || true
+#./scripts/combine.py --zip analyses/2014-Winter-7TeV/2014-Winter-7TeV.py || true
 ./scripts/combine.py --zip analyses/2014-Winter/2014-Winter.py || true
 #./scripts/combine.py --zip analyses/2013-Higgs/2013Higgs.py || true
 #./scripts/combine.py --zip analyses/2012-Paper/2012Paper-new.py || true
