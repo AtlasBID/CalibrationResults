d5b7ecf9aed37eeb15b8b3ce00b0546788b57777
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2014-02-04 02:29:42 +0000

    Ignore errors on builds
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@297362 47dd0619-b7a8-462c-89d8-56395fd97e59

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -2,7 +2,10 @@
 # This script is sourced in the context of the build runner. Place all the configurations you'd like to run
 # here. It is sourced from the base directory of this package.
 #
-./scripts/combine.py --zip analyses/2014-Winter/2014-Winter.py
-./scripts/combine.py --zip analyses/2014-Winter-7TeV/2014-Winter-7TeV.py
-#./scripts/combine.py --zip analyses/2013-Higgs/2013Higgs.py
-#./scripts/combine.py --zip analyses/2012-Paper/2012Paper-new.py
+# The "|| true" at the end of the line allows things to progress - the script doesn't just die.
+
+#
+./scripts/combine.py --zip analyses/2014-Winter/2014-Winter.py || true
+./scripts/combine.py --zip analyses/2014-Winter-7TeV/2014-Winter-7TeV.py || true
+#./scripts/combine.py --zip analyses/2013-Higgs/2013Higgs.py || true
+#./scripts/combine.py --zip analyses/2012-Paper/2012Paper-new.py || true
