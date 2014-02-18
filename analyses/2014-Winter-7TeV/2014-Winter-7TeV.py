0ce3a94322a0fee8efb677de12af42035dfc85b9
coccaro <coccaro@47dd0619-b7a8-462c-89d8-56395fd97e59>
2014-02-18 17:38:17 +0000

    first update of steering file - not properly tested yet
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@297784 47dd0619-b7a8-462c-89d8-56395fd97e59

--- analyses/2014-Winter-7TeV/2014-Winter-7TeV.py
+++ analyses/2014-Winter-7TeV/2014-Winter-7TeV.py
@@ -37,38 +37,99 @@ sfObject.restrict = lambda self: self.filter(
     )
 
 ###############################
-# Bottom inputs
+#### Bottom inputs
 
-# top
+# di-jet input - S8
+s8 = files("system8/*.txt") \
+     .restrict()
 
-ttbar_pdf_6_all = files("ttbar_pdf/6bins/*6bins.txt") \
-                  .restrict() \
-                  .filter(analyses =["ttbar_pdf_emu_2jets_6bins","ttbar_pdf_emu_3jets_6bins","ttbar_pdf_ll_2jets_6bins","ttbar_pdf_ll_3jets_6bins"])
+# di-jet input - pTrel
+ptrel = files ("ptrel/*txt") \
+        .restrict()
 
+# top input - kinematic selection in di-lepton channel
 ttbar_kinsel = files("ttbar_kinsel/*.txt") \
                .restrict()
 
-ttbar_pdf_6_all = ttbar_pdf_6_all.bbb_fit("ttbar_pdf_6_all_fit")
+# top input - PDF with different final states and 6 bins
+ttbar_pdf_6_all = files("ttbar_pdf/6bins/*6bins.txt") \
+                  .restrict() \
+                  .filter(analyses =["ttbar_pdf_emu_2jets_6bins","ttbar_pdf_emu_3jets_6bins","ttbar_pdf_ll_2jets_6bins","ttbar_pdf_ll_3jets_6bins"])
+ttbar_pdf_6_2j = ttbar_pdf_6_all \
+                 .filter(analyses =["ttbar_pdf_emu_2jets_6bins","ttbar_pdf_ll_2jets_6bins"])
+ttbar_pdf_6_3j = ttbar_pdf_6_all \
+                 .filter(analyses =["ttbar_pdf_emu_3jets_6bins","ttbar_pdf_ll_3jets_6bins"])
 
-ttbar = (ttbar_pdf_6_all+ttbar_kinsel).bbb_fit("ttbar_pdf_6_all_fit")
+# top input - PDF with different final states and 10 bins
+ttbar_pdf_10_all = files("ttbar_pdf/10bins/*.txt") \
+                  .restrict() \
+                  .filter(analyses =["ttbar_pdf_emu_2jets","ttbar_pdf_emu_3jets","ttbar_pdf_ll_2jets","ttbar_pdf_ll_3jets"])
+ttbar_pdf_10_2j = ttbar_pdf_10_all \
+                 .filter(analyses =["ttbar_pdf_emu_2jets","ttbar_pdf_ll_2jets"])
+ttbar_pdf_10_3j = ttbar_pdf_10_all \
+                 .filter(analyses =["ttbar_pdf_emu_3jets","ttbar_pdf_ll_3jets"])
 
-sources = ttbar
+# defining sources
+sources = ttbar_pdf_6_all + ttbar_pdf_10_all + ttbar_kinsel
+
+# what we'll be considered the dijet inputs later on
+dijet = s8 + ptrel
+
+# rebin should have the same binning as D*
+rebin_template_all = files("commonbinning.txt") \
+                     .filter(analyses=["rebin"])
+rebin_template = rebin_template_all \
+                 .filter(ignore=[".*300-pt-500.*"])
+rebin_template_30 = files("commonbinning.txt") \
+                    .filter(analyses=["rebin_30"]) \
+                    .filter(ignore=[".*300-pt-500.*"])
+
+# various top-based combination using 6-bin inputs 
+ttbar_pdf_6_combined = ttbar_pdf_6_all.bbb_fit("ttbar_PDF_6b")
+ttbar_pdf_6_combined_2j = ttbar_pdf_6_2j.bbb_fit("ttbar_PDF_6b_2j")
+ttbar_pdf_6_combined_3j = ttbar_pdf_6_3j.bbb_fit("ttbar_PDF_6b_3j")
+
+# various top-based combination using 10-bin inputs 
+ttbar_pdf_10_combined = ttbar_pdf_10_all.bbb_fit("ttbar_PDF_10b")
+ttbar_pdf_10_combined_2j = ttbar_pdf_10_2j.bbb_fit("ttbar_PDF_10b_2j")
+ttbar_pdf_10_combined_3j = ttbar_pdf_10_3j.bbb_fit("ttbar_PDF_10b_3j")
+
+# di-jet combination
+dijet_combined = dijet.bbb_fit("dijet", saveCHI2Fits=True, extraFiles=files("stat_correlation_inputs.txt"))
+
+# all combined together
+ttbar_kinsel_dijet = (ttbar_kinsel + dijet).bbb_fit("ttbar_kinsel_dijet")
+
+# rebinned results used to make charm and tau results 
+ttbar_rebin = (rebin_template + ttbar_pdf_10_combined) \
+              .rebin("rebin", "<>_rebin")
+dijet_rebin = (rebin_template + dijet) \
+              .rebin("rebin", "<>_rebin")
+
+
 
 ###############################
-# Charm Inputs (taus derived from charm too)
+#### Charm Inputs (taus derived from charm too)
 
 dstar_template = files("Dstar/*.txt") \
                  .restrict()
 
-charm_sf = (dstar_template + ttbar) \
-           .dstar("DStar_<>", "DStar")
+charm_sf_ttbar = (dstar_template + ttbar_rebin) \
+                 .dstar("DStar_<>", "DStar")
+
+charm_sf_dijet = (dstar_template + dijet_rebin) \
+                 .dstar("DStar_<>", "DStar")
+
+charm_sf = charm_sf_ttbar + charm_sf_dijet
 
 tau_sf = charm_sf.add_sys("extrapolation from charm", "22%", changeToFlavor="tau")
 
 sources += dstar_template
 
+
+
 ###############################
-# Light quark inputs
+#### Light quark inputs
 
 negative = files("negative_tags/*.txt") \
            .restrict()
@@ -77,27 +138,14 @@ light_sf = negative
 
 sources += negative
 
-###############################
-# Do the extrapolation
-
-#### Warning: filtering out the 20-30 bin is because the extrapolation
-####          code can't yet handle extrapolation on both sides of an axis,
-####          or one where the error gets smaller.
-####          We will need to decide what to do about this.
-#mcCalib = files("MCcalib/*.txt") \
-#    .restrict() \
-#    .filter(ignore=[".*20-pt-30.*"])
-
-#extrapolated = (bottom+mcCalib).extrapolate("MCcalib")
 
 ###############################
-# Put together the CDI
+#### Put together the CDI
 
-master_cdi_file = ttbar \
-                 + charm_sf \
-                 + tau_sf \
-                 + light_sf \
-                 + sources
+master_cdi_file = sources \
+                  + charm_sf \
+                  + tau_sf \
+                  + light_sf
 defaultSFs = master_cdi_file.make_cdi("MC11-CDI", "defaults.txt", "MCefficiencies_for_CDI_16.1.2014_test.root")
 master_cdi_file.plot("MC11-CDI")
 master_cdi_file.plot("MC11-CDI-Tagger-Trends", effOnly=True, byTaggerEff=True)
