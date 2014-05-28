40812ac9f382499e9b853a257123a3ae56e099c6
coccaro <coccaro@47dd0619-b7a8-462c-89d8-56395fd97e59>
2014-05-28 15:29:02 +0000

    new efficiency maps with PowhegP8 and PowhegHerwig++
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@299919 47dd0619-b7a8-462c-89d8-56395fd97e59

--- analyses/2014-Winter/2014-Winter.py
+++ analyses/2014-Winter/2014-Winter.py
@@ -324,7 +324,7 @@ all_extrapolated = default_extrapolated + rebin_extrapolated
 master_cdi_file = \
     light_sf \
     + all_extrapolated
-defaultSFs = master_cdi_file.make_cdi("MC12-CDI", "defaults.txt", "MCefficiencies_for_CDI_8TeV_3.5.2014.root")
+defaultSFs = master_cdi_file.make_cdi("MC12-CDI", "defaults.txt", "StandardTag_8TeV_ttbar_140527141929.root")
 master_cdi_file.plot("MC12-CDI")
 master_cdi_file.plot("MC12-CDI-Tagger-Trends", effOnly=True, byTaggerEff=True)
 master_cdi_file.dump(sysErrors = True, name="master")
