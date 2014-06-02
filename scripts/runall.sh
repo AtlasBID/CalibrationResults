ce45eb7f4a2280092004856186e3889d50375132
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2014-06-02 08:16:19 +0000

    Start running the 7 TeV results again.
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@299996 47dd0619-b7a8-462c-89d8-56395fd97e59

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -5,7 +5,5 @@
 # The "|| true" at the end of the line allows things to progress - the script doesn't just die.
 #
 #
-#./scripts/combine.py --zip analyses/2014-Winter-7TeV/2014-Winter-7TeV.py
+./scripts/combine.py --zip analyses/2014-Winter-7TeV/2014-Winter-7TeV.py
 ./scripts/combine.py --zip analyses/2014-Winter/2014-Winter.py
-#./scripts/combine.py --zip analyses/2013-Higgs/2013Higgs.py || true
-#./scripts/combine.py --zip analyses/2012-Paper/2012Paper-new.py || true
