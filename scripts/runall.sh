65049ac1ed5acc4f64c53789cb0c02aaef643a8f
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2014-07-17 14:38:23 +0000

    = Replace <= by = in the extrapolation files = Turn off 7 TeV results.
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@300736 47dd0619-b7a8-462c-89d8-56395fd97e59

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -5,5 +5,5 @@
 # The "|| true" at the end of the line allows things to progress - the script doesn't just die.
 #
 #
-./scripts/combine.py --zip analyses/2014-Winter-7TeV/2014-Winter-7TeV.py
+#./scripts/combine.py --zip analyses/2014-Winter-7TeV/2014-Winter-7TeV.py
 ./scripts/combine.py --zip analyses/2014-Winter/2014-Winter.py
