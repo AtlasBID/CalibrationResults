c320ea02649a1d5d854f19d59bb040b481afd9bc
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2014-12-01 21:20:49 +0000

    Understanding the ignore... why isn't it working?? :-)
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/CalibrationResults/trunk@302778 47dd0619-b7a8-462c-89d8-56395fd97e59

--- analyses/2012-Paper/2012Paper-new.py
+++ analyses/2012-Paper/2012Paper-new.py
@@ -35,7 +35,7 @@ taggers = [
     ]
 
 ignore_analyses = [
-    "pTrel-.*20-pt-200.*", 
+    "pTrel-.*20-pt-200", 
     ".*:25-pt-30:.*",
 	
 # We have ttbar_pdf for some of the points, so we want to use these instead
