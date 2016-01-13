df457aae6b43413bc4a352f6cd4601b050783b80
AndreaCoccaro <andrea.coccaro@cern.ch>
2016-01-13 16:13:34 +0100

    typo fixed

--- analyses/2016-Winter-13TeV/2016-Winter-13TeV.py
+++ analyses/2016-Winter-13TeV/2016-Winter-13TeV.py
@@ -140,8 +140,8 @@ pre_ttbar_pdf_7_combined = pre_ttbar_pdf_7_combined_withchi2.filter(analyses=["p
 pre_ttbar_pdf_7_combined_2j = pre_ttbar_pdf_7_2j.bbb_fit("pre_ttbar_PDF_7b_2j")
 pre_ttbar_pdf_7_combined_3j = pre_ttbar_pdf_7_3j.bbb_fit("pre_ttbar_PDF_7b_3j")
 
-ttbar_pdf_7_combined_withchi2 = ttbar_pdf_7_all.bbb_fit("ttbar_PDF_7b", saveCHI2Fits=True)
-ttbar_pdf_7_combined = ttbar_pdf_7_combined_withchi2.filter(analyses=["ttbar_PDF_7b_emu_2j3j"])
+ttbar_pdf_7_combined_withchi2 = ttbar_pdf_7_all.bbb_fit("ttbar_PDF_7b_emu", saveCHI2Fits=True)
+ttbar_pdf_7_combined = ttbar_pdf_7_combined_withchi2.filter(analyses=["ttbar_PDF_7b_emu"])
 ttbar_pdf_7_combined_2j = ttbar_pdf_7_2j.bbb_fit("ttbar_PDF_7b_emu_2j")
 ttbar_pdf_7_combined_3j = ttbar_pdf_7_3j.bbb_fit("ttbar_PDF_7b_emu_3j")
 
