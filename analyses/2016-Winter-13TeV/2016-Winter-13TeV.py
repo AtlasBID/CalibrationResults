487ed9cb276c431951d7386cdab790818b69fc38
AndreaCoccaro <andrea.coccaro@cern.ch>
2016-03-07 15:24:10 +0100

    maps for the MC/MC SF for the b-jet SF on R=0.2 track-jets

--- analyses/2016-Winter-13TeV/2016-Winter-13TeV.py
+++ analyses/2016-Winter-13TeV/2016-Winter-13TeV.py
@@ -333,7 +333,7 @@ sf_trackjets = b_trackjets_extrap + ct_trackjets_extrap + negative_trackjets_ext
 #
 
 master_cdi_file = all_extrapolated+sf_trackjets
-defaultSFs = master_cdi_file.make_cdi("MC15-CDI", "defaults.txt","StandardTag-13TeV-prerecommendationCalibrationFile5-160304175044.root","BtagWP-Feb2016-V2.root")
+defaultSFs = master_cdi_file.make_cdi("MC15-CDI", "defaults.txt","StandardTag-13TeV-prerecommendationCalibrationFile5-160307135509.root","BtagWP-Feb2016-V2.root")
 master_cdi_file.plot("MC15-CDI", effOnly=True)
 master_cdi_file.dump(linage=True, name="master-cdi-linage")
 master_cdi_file.plot("MC15-CDI-Tagger-Trends", effOnly=True, byTaggerEff=True)
